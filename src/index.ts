7d3af182de9fd2b7c3b67bd0b4751e9bd8d2f9ce
Dok6857 <Dok6857@gmail.com>
2024-04-08 19:42:52 +0300

    set up tsc

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function add(num1: number, num2: number) {
+    return num1 + num2;
+  }
+  
+  add(1, 1);
+  
+  let fullName: string = '';
+  let age = 12;
+
+//   const something: boolean[] = [3, 'something', true]
+
+type user = {}
